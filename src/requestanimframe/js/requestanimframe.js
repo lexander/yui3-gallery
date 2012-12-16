acb894b8af41b207d5a98d95abc80c2200b31a14
Alexander <alexander.shusta@gmail.com>
2012-12-15 20:10:33 -1000

    Updated docs and renamed method to cancelAnimFrame to match browser API more closely.

--- src/requestanimframe/js/requestanimframe.js
+++ src/requestanimframe/js/requestanimframe.js
@@ -6,7 +6,7 @@ requestAnimationFrameFunction = (function() {
     if(window.requestAnimationFrame){
         return window.requestAnimationFrame;
     }
-    // vendor prefixed
+    // vendor prefixed?
     for(x = 0; x < vendors.length; ++x) {
         if(typeof window[vendors[x]+'RequestAnimationFrame'] === "function"){
             return window[vendors[x]+'RequestAnimationFrame'];
@@ -26,17 +26,21 @@ requestAnimationFrameFunction = (function() {
 
 }());
 
-cancelRequestAnimationFrameFunction = (function() {
+cancelAnimationFrameFunction = (function() {
     var vendors = ['ms', 'moz', 'webkit', 'o'],
         x;
 
+    // native?
+    if(window.cancelAnimationFrame){
+        return window.cancelAnimationFrame;
+    }
+    // vendor prefixed?
     for(x = 0; x < vendors.length; ++x) {
-        if(typeof window[vendors[x]+'CancelRequestAnimationFrame'] === "function"){
-            return window[vendors[x]+'CancelRequestAnimationFrame'];
+        if(typeof window[vendors[x]+'CancelAnimationFrame'] === "function"){
+            return window[vendors[x]+'CancelAnimationFrame'];
         }
     }
-
-    
+    // fallback 
     return function(id) {
         clearTimeout(id);
     };
@@ -49,6 +53,8 @@ cancelRequestAnimationFrameFunction = (function() {
  * @method requestAnimFrame
  * @param callback {function} A parameter specifying a function to call when
  * it's time to update your animation for the next repaint.
+ * @return requestId {number} uniquely identifies the request, pass
+ * this to cancelAnimFrame to skip drawing a previously scheduled frame.
  */
 // Need to bind the context appropriately or the browser will
 // not know what scope to execute the function in and will error out.
@@ -70,11 +76,10 @@ Y.requestAnimFrameWithScope = function(fn, context) {
 };
 
 /**
- * CancelRequestAnimationFrame polyfill hooked into the top-level YUI object.
+ * CancelAnimationFrame polyfill hooked into the top-level YUI object.
  * Adapted from
  * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- * @method cancelRequestAnimFrame
- * @param id {string} animation id to cancel, if using the fallback setTimeout based
- * animation.
+ * @method cancelAnimFrame
+ * @param id {number} animation id to cancel
  */
-Y.cancelRequestAnimFrame = Y.bind(cancelRequestAnimationFrameFunction, window);
+Y.cancelAnimFrame = Y.bind(cancelAnimationFrameFunction, window);
